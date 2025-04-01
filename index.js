aa9153db6631f314701ce8856e7da3716317cbdf
Lucas Almeida <lucas-lxx@alu.ufc.br>
2025-04-01 13:32:41 -0300

    feat: first view configured to render

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ app.set('views', 'views');
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('./public/'));
+
+app.use('/', (req, res, next) => {
+  res.render('index');
+});
+
+app.listen(process.env.PORT, process.env.HOST, res => {
+  console.log(`app listening on port: ${process.env.PORT}`)
+});
