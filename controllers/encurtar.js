0e46fba1b9e1f9c7e49568f02b95fb61f62a8495
Lucas Almeida <lucas-lxx@alu.ufc.br>
2025-04-07 15:33:13 -0300

    feat: return short link created

--- controllers/encurtar.js
+++ controllers/encurtar.js
@@ -1,12 +1,34 @@
+const path = require('path');
+const { nanoid } = require('nanoid');
+
+const Link = require('../models/link');
+
 exports.getIndex = (req, res, next) => {
   res.render('index');
 };
 
 exports.createShortLink = (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/link/success');
+  const originalLink = req.body.originalLink;
+  Link.findOne({where: {original_link: originalLink}})
+  .then(data => {
+    const randomPath = nanoid(7);
+    const shortLinkName = path.join(process.env.URL, randomPath);
+    console.log("originalLink: ", originalLink);
+    console.log("shortLink: ", shortLinkName);
+    return Link.create({
+      original_link: originalLink,
+      short_link: shortLinkName,
+      last_click: null,
+      clicks: 0
+    })
+  })
+  .then(shortLink => {
+    console.log("short obj", shortLink);
+    res.json({link: shortLink.short_link});
+  })
+  .catch(err => {console.error(err); res.json({link: 'server error: function createShortLink()'})});
 };
 
 exports.createLinkSuccess = (req, res, next) => {
-  res.send('success!');
+  res.send({link: 'success'});
 }
