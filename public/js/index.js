9731284066d4946b8f55f92b309ea22fde7f7dfb
Lucas Almeida <lucas-lxx@alu.ufc.br>
2025-04-15 16:47:28 -0300

    refactor: update the naming

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
 // import axios from "axios";
 
-const originalLinkElement = document.getElementById('originalLinkForm');
+const originalLinkElement = document.getElementById('link__form');
 
 const createElement = async (element_name, options = {classes: [], id: null, value: null}) => {
   let element = document.createElement(element_name);
@@ -11,31 +11,29 @@ const createElement = async (element_name, options = {classes: [], id: null, val
 }
 
 const generateShortLinkContainer = async (data) => {
-  data = data.data;
   const anchorTagClass = 'short-link__anchor-container';
-  let form = document.getElementById('originalLinkForm');
-  let div = document.getElementById('shortLinkDiv');
+  let form = document.getElementById('link__form');
+  let div = document.getElementById('link__short-link-container')
   if (!div) {
     div = document.createElement('div');
-    div.id = 'shortLinkDiv';
+    div.classList.add = 'link__short-link-container';
     let a = document.createElement('a');
-    a.id = 'shortLink';
+    a.id = 'link__short-link';
     div.appendChild(a);
     form.append(div);
   }
-  let a = document.getElementById('shortLink');
+  let a = document.getElementById('link__short-link');
   a.innerText = data.link;
   a.href = data.link;
   a.classList.add(anchorTagClass);
   a.target = '_blank';
-  console.log(div);
 }
 
 const returnShortLink = async (event) => {
   event.preventDefault();
   const data = { originalLink: document.getElementById('originalLink').value};
   const axios_data = await axios.post('/link', data, {'Content-Type': 'application/json; charset=UTF-8'});
-  generateShortLinkContainer(axios_data);
+  generateShortLinkContainer(axios_data.data);
 } 
 
 originalLinkElement.addEventListener('submit', returnShortLink);
