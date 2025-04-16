21a1198339265135ce375db4382dd8db839a954b
Lucas Almeida <lucas-lxx@alu.ufc.br>
2025-04-15 21:20:05 -0300

    refactor: update for acoplation

--- public/js/index.js
+++ public/js/index.js
@@ -18,12 +18,12 @@ const generateShortLinkContainer = async () => {
   link_block.append(short_link_container);
 }
 
-const updateShortLink = async (data) => {
+const updateShortLink = async (link) => {
   let short_link_container = document.getElementById('link__short-link-container')
   if (!short_link_container) { short_link_container = await generateShortLinkContainer(); }
   let short_link = document.getElementById('link__short-link');
-  short_link.innerText = data.link;
-  short_link.href = data.link;
+  short_link.innerText = link;
+  short_link.href = link;
   short_link.target = '_blank';
 }
 
@@ -31,7 +31,7 @@ const returnShortLink = async (event) => {
   event.preventDefault();
   const data = { original_link: document.getElementById('original_link').value};
   const axios_data = await axios.post('/link', data, {'Content-Type': 'application/json; charset=UTF-8'});
-  updateShortLink(axios_data.data);
+  updateShortLink(axios_data.data.link);
 } 
 
 originalLinkElement.addEventListener('submit', returnShortLink);
