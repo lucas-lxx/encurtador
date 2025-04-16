41d505304ada9db215f301ae1e0c6ebffd4b5d48
Lucas Almeida <lucas-lxx@alu.ufc.br>
2025-04-15 22:46:45 -0300

    refactor: change from .then to async/await

--- controllers/encurtar.js
+++ controllers/encurtar.js
@@ -7,34 +7,38 @@ exports.getIndex = async (req, res, next) => {
   res.render('index');
 };
 
-exports.createShortLink = async (req, res, next) => {
+exports.createShortLink = async (req, res, next, tryy = null) => {
+  if (!tryy) tryy = 3;
   const originalLink = req.body.original_link;
-  Link.findAll({where: {original_link: originalLink}})
-  .then(data => {
-    const shortLinkId = nanoid(7);
-    const shortLinkName = getShortLinkName(shortLinkId);
-    return Link.create({
+  const shortLinkId = nanoid(7);
+  const shortLinkName = getShortLinkName(shortLinkId);
+  try {
+    const link = await Link.create({
       original_link: originalLink,
       short_link: shortLinkName,
       last_click: null,
       clicks: 0
-    })
-  })
-  .then(shortLink => {
-    console.log("short obj", shortLink);
-    res.json({link: shortLink.short_link});
-  })
-  .catch(err => {console.error(err); res.json({link: 'server error: function createShortLink()'})});
+    });
+    res.json({link: link.short_link});
+  } catch (e) {
+    console.error(e);
+    if (tryy <= 0) res.status(500).json({error: 'Internal server error'});
+    if (e.name === 'SequelizeUniqueConstraintError') {
+      this.createShortLink(req, res, next, (tryy-1));
+    }
+  }
 };
 
 exports.getShortLink = async (req, res, next) => {
   const shortLinkName = getShortLinkName(req.params.short_link_id);
-  Link.findByPk(shortLinkName)
-  .then(link => {
+  const link = await Link.findByPk(shortLinkName);
+  try {
     link.last_click = new Date();
     link.clicks += 1;
     link.save();
     res.redirect(link.original_link);
-  })
-  .catch(err => console.error(err));
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({error: 'Internal server error'});
+  }
 }
