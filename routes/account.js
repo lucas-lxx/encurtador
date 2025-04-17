c887404a7eea779121ed0a508b2b8acc08fa9cd8
Lucas Almeida <lucas-lxx@alu.ufc.br>
2025-04-17 18:52:09 -0300

    refactor: modify routes for better understanding

--- routes/account.js
+++ routes/account.js
@@ -4,6 +4,18 @@ const UserController = require('../controllers/user');
 
 router.post('/signup', UserController.createUser);
 
-router.get('/login', UserController.getLogin);
+router.get('/signup', async (req, res, next) => {
+  res.render('signup-login', {
+    path: '/account/signup'
+  })
+});
+
+router.post('/login', UserController.getLogin);
+
+router.get('/login', async (req, res, next) => {
+  res.render('signup-login', {
+    path: '/account/login'
+  })
+});
 
 module.exports = router;
