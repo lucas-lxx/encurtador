c63ddb2640104b9d80d21fc57b777c89067fa538
Lucas Almeida <lucas-lxx@alu.ufc.br>
2025-04-28 17:36:22 -0300

    refactor: add input of original link functionality

---
+++ frontend/src/components/LinkInput.vue
@@ -0,0 +1,24 @@
+<script setup>
+import axios from 'axios';
+import { ref } from 'vue';
+
+const original_link = ref('');
+
+async function submitForm(event) {
+  try {
+    const data = { original_link: original_link.value};
+    const axios_data = await axios.post('http://127.0.0.1:3000/link', data, {'Content-Type': 'application/json; charset=UTF-8'});
+    console.log(axios_data);
+  } catch (e) {
+    console.log(e)
+  }
+}
+</script>
+
+<template>
+  <form @submit.prevent="submitForm">
+    <label for="original_link">Link para encurtar</label>
+    <input type="text" name="original_link" v-model="original_link">
+    <button type="submit">encurtar</button>
+  </form>
+</template>
